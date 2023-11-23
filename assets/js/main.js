77a142e1fef83f7fcb09dc2f347624d66024f296
ShuklaGarvita <100278607+ShuklaGarvita@users.noreply.github.com>
2023-11-23 19:46:52 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,41 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+
+
+// popup
+var $box = $(".box");
+
+$(".closei").each(function () {
+  var color = $(this).css("backgroundColor");
+  var content = $(this).html();
+  $(this).click(function () {
+    $box.css("backgroundColor", color);
+    $box.addClass("open");
+    $box.find("p").html(content);
+  });
+
+  $(".close").click(function () {
+    $box.removeClass("open");
+    $box.css("backgroundColor", "transparent");
+  });
+
+  $("body").toggleClass("overlay");
+  $("#pop-toggle").click(function () {
+    $(".popup").toggle();
+    $("body").toggleClass("overlay");
+  });
+  $(".close").click(function () {
+    $(".popup").toggle();
+    $("body").toggleClass("overlay");
+  });
+});
+
+// popup closed
+
+
+
 (function() {
   "use strict";
 
